829ee713665e804ff47ff290939b01e30ebd2cc0
didaryalcin <ddrylcn@gmail.com>
2023-12-31 01:37:07 +0300

    1commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,30 @@
 export const ADD_ONE = "ADD_ONE";
-
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
-
+export const MPLUS = "MPLUS";
+export const MEMORYR = "MEMORYR";
+export const MEMORYC = "MEMORYC";
 export const addOne = () => {
-  return ({ type: ADD_ONE });
-}
+  return { type: ADD_ONE };
+};
 
 export const applyNumber = (number) => {
-  return ({ type: APPLY_NUMBER, payload: number });
-}
+  return { type: APPLY_NUMBER, payload: number };
+};
+
+export const clearDisplay = () => {
+  return { type: CLEAR_DISPLAY };
+};
+
+export const mPlus = () => {
+  return { type: MPLUS };
+};
+
+export const memoryR = () => {
+  return { type: MEMORYR };
+};
+
+export const memoryC = () => {
+  return { type: MEMORYC };
+};
